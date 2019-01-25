6c4abdc03af6c8a11a248ef3736581150a7ae59f
bstelly <bstelly1999@gmail.com>
2019-01-25 17:52:57 -0600

    Working on NPC

---
+++ Assets/Scripts/NpcBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcBehaviour : MonoBehaviour
+{
+    bool isDead;
+    
+
+    void Start() 
+    {
+        isDead = false;
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
