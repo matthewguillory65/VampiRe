79fb8fb7d062b1bc64261e5ee639a166f2989abf
bstelly <bstelly1999@gmail.com>
2019-02-05 12:32:37 -0600

    npcs can detect player
    Distance of view and angle of view is able to be set in the inpsector.

--- Assets/Scripts/detection.cs
+++ Assets/Scripts/detection.cs
@@ -2,54 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NpcBehaviour : MonoBehaviour
+public class detection : MonoBehaviour
 {
     public Transform player;
     public float angleOfView;
     public float distanceOfView;
-    public bool isDead;
-    public bool rightShoulderGrabbed;
-    public bool leftShoulderGrabbed;
-    Animator anim;
     RaycastHit hit;
-
-    void Start() 
-    {
-        anim = GetComponent<Animator>();
-        isDead = false;
-	}
-
-    void Update ()
-    {
-
-        PlayerDetection();
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            leftShoulderGrabbed = true;
-        }
-        else
-        {
-            leftShoulderGrabbed = false;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rightShoulderGrabbed = true;
-        }
-        else
-        {
-            rightShoulderGrabbed = false;
-        }
-        if (Input.GetKey(KeyCode.Space))
-        {
-            isDead = true;
-            anim.SetBool("isDead", true);
-        }
-	}
-
     public bool ICanSeeThePlayer = false;
 
-    void PlayerDetection()
+    // Update is called once per frame
+    void Update()
     {
         var targetDir = player.position - transform.position;
         float angle = Vector3.Angle(targetDir, transform.forward);
@@ -60,7 +22,7 @@ public class NpcBehaviour : MonoBehaviour
             Debug.DrawLine(transform.position, player.transform.position, Color.blue);
             if (Physics.Linecast(transform.position, player.transform.position, out hit))
             {
-                if (hit.collider.CompareTag("Player"))
+                if(hit.collider.CompareTag("Player"))
                 {
                     print("NPC can see player");
                     ICanSeeThePlayer = true;
