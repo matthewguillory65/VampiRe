cc964edd6fa2280e8a3e2fa75350c4ae22ee8d72
Ralenski Doucet <Ralenskid@gmail.com>
2019-02-07 11:53:43 -0600

    commit
    Started Get Caught Event system

--- Assets/Scripts/Caughtevent.cs
+++ Assets/Scripts/Caughtevent.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Caughtevent : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    public GameEvent OnCaughtEvent;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
