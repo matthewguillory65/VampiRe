3c2418c3876888284ec6c0f38404b7da7fc55b04
Ralenski Doucet <Ralenskid@gmail.com>
2019-02-07 15:38:10 -0600

    finished On Caught Event

---
+++ Assets/Scripts/SkirtBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkirtBehaviour : MonoBehaviour {
+    public GameEvent OnCaughtEvent;
+    public GameEvent OnNotCaughtEvent;
+    public Renderer renderer;
+
+    // Use this for initialization
+    
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        OnCaughtEvent.Raise();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        OnNotCaughtEvent.Raise();
+    }
+    public void ChangeColor()
+    {
+        if(renderer.material.color == Color.green)
+        {
+            renderer.material.color = Color.red;
+        }
+        else
+        {
+            renderer.material.color = Color.green;
+        }
+    }
+
+
+}
