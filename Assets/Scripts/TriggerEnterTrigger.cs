d150d879944f0851d1578375b00859d62cd302b2
Ralenski Doucet <Ralenskid@gmail.com>
2019-02-21 10:54:56 -0600

    Cutscene Trigger
    Made a script that will trigger on trigger events.

---
+++ Assets/Scripts/TriggerEnterTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Collider))]
+
+public class TriggerEnterTrigger : MonoBehaviour
+{
+    public string compareTag;
+    public GameEvent gameEvent;
+   
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag(compareTag))
+        {
+            gameEvent.Raise();
+        }
+    }
+
+}
