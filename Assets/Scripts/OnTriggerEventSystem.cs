8610b1dc1839e679cd010c0ce0326949aa751ad1
Ralenski Doucet <Ralenskid@gmail.com>
2019-02-26 17:52:17 -0600

    Commit
    Deleted old cut scene trigger script

---
+++ Assets/Scripts/OnTriggerEventSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[RequireComponent(typeof(Collider))]
+public class OnTriggerEventSystem : MonoBehaviour
+{
+    [FormerlySerializedAs("PlayerTag")] public string playerTag;
+    [FormerlySerializedAs("TriggerEnter")] public GameEvent triggerEnter;
+    [FormerlySerializedAs("TriggerEnd")] public GameEvent triggerEnd;
+    [FormerlySerializedAs("TriggerStay")] public GameEvent triggerStay;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTag)==true)
+        {
+            triggerEnter.Raise();
+            Debug.Log("Trigger Start.");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(playerTag)==true)
+        {
+            triggerEnd.Raise();
+            Debug.Log("Trigger Stop.");
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag(playerTag)== true)
+        {
+            triggerStay.Raise();
+        }
+    }
+}
