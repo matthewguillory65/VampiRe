3631fe40c93dfa6396234b9975e1ed9fba82eee5
Ralenski Doucet <Ralenskid@gmail.com>
2019-02-26 17:54:55 -0600

    Commit
    Started a new Editor Window script to allow the toggoleing of trigger systems.

---
+++ Assets/Scripts/EditorWindowScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+[CustomEditor(typeof(OnTriggerEventSystem))]
+public class EditorWindowScript : Editor {
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (EditorGUILayout.Toggle("On Trigger Enter", false))
+        {
+
+        }
+        if (EditorGUILayout.Toggle("On Trigger Exit", false))
+        {
+            
+        }
+        if (EditorGUILayout.Toggle("On Trigger Stay", false))
+        {
+
+        }
+    }
+ 
+    
+
+}
