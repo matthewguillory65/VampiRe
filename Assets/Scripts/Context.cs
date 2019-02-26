091fa03d50dff5306e71f430b9c118dc0229461f
bstelly <bstelly1999@gmail.com>
2019-02-26 17:55:41 -0600

    Began working on GameState system

---
+++ Assets/Scripts/Context.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace Assets.Scripts.Brett
+{
+
+    public class Context : MonoBehaviour
+    {
+        public GameState Current;
+
+        void Update()
+        {
+
+        }
+
+        public void ChangeState(GameState s)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Brett
+{
+
+    public class GameState : MonoBehaviour
+    {
+
+        private void OnEnter()
+        {
+
+        }
+
+        private void OnExit()
+        {
+
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
