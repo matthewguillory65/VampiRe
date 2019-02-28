49e3a89a5d94c5dec0864180f302d633c236d6c8
matthewguillory65 <matthewguillory65@gmail.com>
2019-02-28 16:51:07 -0600

    Grabbing works finishing assigned work

---
+++ Assets/Scripts/Class1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+namespace Assets.Scripts
+{
+    public class TestStaticReferenceBehaviour : MonoBehaviour
+    {
+        public string left, right, sphere;
+        private void Start()
+        {
+            left = HandsReference.LeftHand.name;
+            right = HandsReference.RightHand.name;
+            sphere = HandsReference.Sphere.name;
+        }
+
+        private void Update()
+        {
+            left.CompareTo(sphere);
+            
+        }
+    }
+}
