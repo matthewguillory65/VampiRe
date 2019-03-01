e6fba34cb9d7cc785f0583e5741cb6ede7d2162c
Ralenski Doucet <Ralenskid@gmail.com>
2019-03-01 13:50:56 -0600

    commit
    Made changes to my Editor script

--- Assets/Scripts/EditorWindowScript.cs
+++ Assets/Scripts/EditorWindowScript.cs
@@ -1,25 +1,55 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
 using UnityEngine;
-
-[CustomEditor(typeof(OnTriggerEventSystem))]
-public class EditorWindowScript : Editor
+[CustomEditor(typeof(EditorGUILayout)) ]
+public class EditorWindowScript  :  Editor
 {
     public bool onTriggerEnterToggle;
     public bool onTriggerExitToggle;
     public bool onTriggerStayToggle;
-
     public override void OnInspectorGUI()
     {
         onTriggerEnterToggle = EditorGUILayout.Toggle("On Trigger Enter", onTriggerEnterToggle);
-        if (onTriggerEnterToggle == true)
-        {
-            
-        }
-       onTriggerExitToggle = EditorGUILayout.Toggle("On Trigger Exit", onTriggerExitToggle);
+        onTriggerExitToggle = EditorGUILayout.Toggle("On Trigger Exit", onTriggerExitToggle);
         onTriggerStayToggle = EditorGUILayout.Toggle("On Trigger Stay", onTriggerStayToggle);
         base.OnInspectorGUI();
-
-
     }
+    private void EventListener(GameEvent gameEvent, int index)
+    {
+        var fieldingInfo = gameEvent.GetType().GetField("Listeners",BindingFlags.Instance);
+        if (fieldingInfo != null)
+        {
+            var i = fieldingInfo.GetValue(gameEvent);
+            var listeners = i as List<GameEventListener>;
+            GUILayout.BeginVertical(EditorStyles.helpBox);
+            if (listeners != null && listeners.Count <= 10)
+            {
+                EditorGUILayout.LabelField(gameEvent.name);
+                EditorGUILayout.LabelField("listeners: 10");
+            }
+            else
+            {
+                var width = EditorStyles.miniButton.CalcSize(new GUIContent("Raise"));
+                EditorGUILayout.BeginHorizontal(EditorStyles.toolbar);
+                EditorGUILayout.LabelField(gameEvent.name);
+                if (GUILayout.Button("Raise", EditorStyles.toolbarButton, GUILayout.Width(width.x + 4)))
+                    gameEvent.Raise();
+                EditorGUILayout.EndHorizontal();
 
+                EditorGUI.indentLevel++;
+                if (listeners != null)
+                    foreach (var listener in listeners)
+                    {
+                    }
+
+                EditorGUI.indentLevel--;
+            }
+        }
+
+        GUILayout.EndVertical();
+    }
+    private GUIContent[] mContents;
+    private List<GameEvent> mGameEvents = new List<GameEvent>();
+  
 }
