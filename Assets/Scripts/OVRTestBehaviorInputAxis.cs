e2bdc26686f092d7161c583ba92d04ab7e1328f3
matthewguillory65 <matthewguillory65@gmail.com>
2019-03-22 16:07:15 -0500

    OVRInput implementation

---
+++ Assets/Scripts/OVRTestBehaviorInputAxis.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OVRTestBehaviorInputAxis : MonoBehaviour
+{
+
+    public GameObject collidingObject, objectInHand;
+    public float TestAxis;
+    public string inputName = "PrimaryControllerHandTrigger";
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        TestAxis = Input.GetAxis(inputName);
+    }
+}
